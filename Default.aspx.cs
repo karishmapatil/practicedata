e798f265a59f5321acd804f8328b1bf5820f2629
karishmapatil <karishma.patil@tops-int.com>
2016-04-18 03:40:10 -0700

    Added files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    DataTable dt;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            fillgrid();
+        }
+    }
+
+    private void fillgrid()
+    {
+        da = new SqlDataAdapter("select * from stud", con);
+        dt = new DataTable();
+        da.Fill(dt);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+}
